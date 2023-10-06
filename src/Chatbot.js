90f5bc4abec0224c14de4ffc95133a425a55891f
sri_ganesh30 <sgthegammerdevil@gmail.com>
2023-10-06 19:57:24 +0530

    Initial commit

---
+++ src/Chatbot.js
@@ -0,0 +1,38 @@
+import  { useEffect } from 'react';
+
+const ChatBot = () => {
+
+
+    useEffect(() => {
+        const script = document.createElement('script');
+
+        script.innerHTML = `
+      (function (w, d, s, o, f, js, fjs) {
+        w["botsonic_widget"] = o;
+        w[o] =
+          w[o] ||
+          function () {
+            (w[o].q = w[o].q || []).push(arguments);
+          };
+        (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+        js.id = o;
+        js.src = f;
+        js.async = 1;
+        fjs.parentNode.insertBefore(js, fjs);
+      })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
+      Botsonic("init", {
+        serviceBaseUrl:${process.env.REACT_APP_serviceBaseUrl},
+        token: ${process.env.REACT_APP_token},
+      });
+    `;
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
+
+};
+
+export default ChatBot;
